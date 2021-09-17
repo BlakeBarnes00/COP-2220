2293fca102bfec52b96d2bdbb64f63a99d3356ad
Ellis Barnes <ellisbarnes00@gmail.com>
2021-09-17 02:55:04 -0400

    Completed 3.5 on MPL

---
+++ CH 3 - Structured Program Development in C/3.5 - The if Selection Statement/10061.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// Values defined by MPL
+int goodsSold = 403023;
+
+int main(){
+	int bonus;
+	if (goodsSold > 500000) {
+		bonus = 10000;
+	}
+	return 0;
+}
---
+++ CH 3 - Structured Program Development in C/3.5 - The if Selection Statement/10062.c
@@ -0,0 +1,10 @@
+// variables defined by MPL
+int outsideTemperature = 91;
+
+int main() {
+	int shelfLife;
+	if (outsideTemperature > 90) {
+		shelfLife -= 4;
+	}
+	return 0;
+}
