bf79080ebe138e13b25f30eb53bf99b8d61d1802
Ellis Barnes <ellisbarnes00@gmail.com>
2021-09-22 18:49:12 -0400

    Finished 4.4 Assignments

---
+++ CH 4 - C Program Control/4.4 for Iteration Statement/10072.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+	for(int k = 0; k < 97; k++) {
+		printf("*");
+	}
+	return 0;
+}
---
+++ CH 4 - C Program Control/4.4 for Iteration Statement/10073.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+// MPL variables
+int n = 1100;
+int j;
+
+int main() {
+	for(j = 1; j <= n; j++) {
+		//printf("%i: *\n", j);
+		printf("*");
+	}
+	return 0;
+}
